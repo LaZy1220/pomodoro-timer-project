3b71e6a56b9d8cae6797c5e98bea81f3ebf469a4
LaZy1220 <4elove4iska@gmail.com>
2022-07-06 18:19:39 +0300

    Add timer and buttons

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,9 +1,29 @@
+import { CircularProgressbar,buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { PauseButton } from '../PauseButton/PauseButton';
+import { SettingsButton } from '../SettingsButton/SettingsButton';
+import { StartButton } from '../StartButton/StartButton';
 import './Timer.css'
 
 export function Timer(){
     return(
-        <div>
-            timer
+        <div className='timer'>
+            <CircularProgressbar
+             value={50} 
+             text={'50%'}
+             styles={buildStyles({rotation:0, 
+                strokeLinecap: 'butt',
+                textColor:'white',
+                pathColor:'red',
+                trailColor:'rgba(255,255,255,.2'
+            })}/>
+            <div className='timer-btn-wrapper'>
+                <StartButton/>
+                <PauseButton/>
+            </div>
+            <div style={{marginTop:'20px'}}>
+                <SettingsButton/>
+            </div>
         </div>
     )
 }
